774822aa38528d886b97555f9ae72f9c2c6fae49
Farzam <107337733+farzam313@users.noreply.github.com>
2023-09-04 14:04:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const router = require("./routes/Data_insert");
+const login = require("./routes/login");
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with the allowed origin(s)
+    methods: "POST , PATCH , OPTIONS", // Allowed HTTP methods
+    allowedHeaders: "Content-Type", // Allowed headers
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
 
-app.use(router);
+app.use(router, (res, err) => {});
+app.use(login, () => {});
+// app.use((req, res, next) => {
+//   // res.setHeader("Access-Control-Allow-Origin", "*");
+//   const headers = {
+//     "Content-Type": "application/json",
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
+//     "Access-Controll-Allow-Headers": "Content-Type",
+//     "Access-Controll-Allow-Origin": "http://localhost:3001",
+//   };
+//   res.setHeader(headers);
 
-app.listen(3000, () => {
-  console.log("Server is started at port: 3000 ");
+//   next();
+// });
+
+app.listen(5000, () => {
+  console.log("Server is Running on port number: 5000 ");
 });
